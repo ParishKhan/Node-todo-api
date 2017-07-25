eb1dfe79ac51a60d6290a83c5269189368c24ebf
PariSh KhAn <xttrra@gmail.com>
2017-07-26 01:06:20 +0600

    fix wrong port variable

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ var {Users} = require('./models/users');
 var app = express(); // New Express App
 
 // Env Variables
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(bodyParser.json());
