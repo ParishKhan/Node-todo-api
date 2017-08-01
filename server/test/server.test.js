5892b85578256a9c3c2f9eeb64e3e84638e7320b
PariSh KhAn <xttrra@gmail.com>
2017-08-01 23:23:05 +0600

    Add PATCH /todos/id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -11,7 +11,9 @@ var todosTest = [{
 },
 {
   _id: new ObjectID,
-  text: "Second test todo"
+  text: "Second test todo",
+  completed: true,
+  completedAt: 333
 }]
 
 beforeEach((done) => {
@@ -140,4 +142,58 @@ describe('DELETE /todos/:id', () => {
       .expect(404)
       .end(done);
   });
+});
+
+describe('PATCH /todos/:id', () => {
+  it('Should Update a todo', (done) => {
+    var id = todosTest[0]._id.toHexString();
+    var updateTodo = {
+      "text": "should updated",
+      "completed": true
+    }
+    
+    request(app)
+      .patch(`/todos/${id}`)
+      .send(updateTodo)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(updateTodo.text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end((err, res) => {
+        Todos.findById(id).then((todo) => {
+          expect(todo.text).toBe(updateTodo.text);
+          expect(todo.completed).toBe(true);
+          expect(todo.completedAt).toBeA('number');
+          done()
+        }).catch((err) => done(err));
+      })
+  });
+
+  it('Should clear completedAt when todo is not completed', (done) => {
+    var id = todosTest[1]._id.toHexString();
+    var updateTodo = {
+      "text": "should updated",
+      "completed": false
+    }
+
+    request(app)
+      .patch(`/todos/${id}`)
+      .send(updateTodo)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(updateTodo.text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toBe(null);
+      })
+      .end((err, res) => {
+        Todos.findById(id).then((todo) => {
+          expect(todo.text).toBe(updateTodo.text);
+          expect(todo.completed).toBe(false);
+          expect(todo.completedAt).toBe(null);
+          done()
+        }).catch((err) => done(err));
+      })
+  })
 })
